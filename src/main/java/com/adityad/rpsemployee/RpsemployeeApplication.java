bea373890a78c1c57ee159574e5d988be601fe5b
F1oppy <dadheech.aditya041@gmail.com>
2022-06-14 10:44:10 +0530

    Initial commit

---
+++ src/main/java/com/adityad/rpsemployee/RpsemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.adityad.rpsemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpsemployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adityad/rpsemployee/RpsemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityad.rpsemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
